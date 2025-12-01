7e06832b87ecce64c9d3477fc44271a59bd2a6c0
Tisha <sumaiyataslim43@gmail.com>
2025-12-02 01:21:27 +0600

    added another script file for slideshow images

---
+++ script/index.js
@@ -0,0 +1,8 @@
+let index = 0;
+const slider = document.getElementById("slider");
+const total = slider.children.length;
+
+setInterval(() => {
+  index = (index + 1) % total;
+  slider.style.transform = `translateX(-${index * 100}%)`;
+}, 2000);
